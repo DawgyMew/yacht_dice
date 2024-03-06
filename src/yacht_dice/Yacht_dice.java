e519b45cefe54b4bb257564d5a6ed491ed415c23
Dawgy <160785597+DawgyMew@users.noreply.github.com>
2024-03-06 17:20:46 -0600

    Initial commit

---
+++ src/yacht_dice/Yacht_dice.java
@@ -0,0 +1,145 @@
+package yacht_dice;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Yacht Dice
+ * @author Kat
+ * Recreation of the hit game yacht dice in java
+ * Work in Progress
+ * :3
+ */
+public class Yacht_dice extends scoring{
+    public static void main(String[] args) {
+        // define variables
+        Scanner sc = new Scanner(System.in);
+        //scoring.test(sc);
+        final var MAX_ROLL_COUNT = 3;
+        int[] savedNums = {0, 0, 0, 0, 0};// array for numbers the user saves
+        int[] finalRolls = {0, 0, 0, 0, 0}; // array to save the final rolls because it wont let me define newRolls outside of the loop and im going to meow so loud.
+        int[] scoreSheet = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        // scoreSheetStatus keeps track of if the score has been written to.
+        boolean[] scoreSheetStatus = {false, false, false, false, false, false, false, false, false, false, false, false};
+        //scoring.test(sc);
+        //scoring.chooseScore(scoreSheetStatus, sc);
+        // For 1 turn
+        for (int i = 0; i < MAX_ROLL_COUNT; i++){
+            int[] newRolls = {0, 0, 0, 0, 0}; // Create empty array with 5 empty slots
+            for (int j = 0; j < (5 - checkSaved(savedNums)); j++){
+                var newNum = rollDice();
+                newRolls[j] = newNum;
+            }
+            //showNewRolls(newRolls);
+            savedNums = saveRoll(newRolls, savedNums, sc);
+            savedNums = removeSavedNum(savedNums, sc);
+            //System.out.println(Arrays.toString(savedNums));
+            finalRolls = newRolls;
+        }
+        var finalNumbers = combineArrays(savedNums, finalRolls);
+        System.out.println(Arrays.toString(finalNumbers));
+        var scoreArray = scoring.generateScores(finalNumbers, scoreSheetStatus); // possible scores to get
+        var catChoice = scoring.chooseScore(scoreSheetStatus, sc);
+        sc.close();
+    }
+    
+    // Get a random number from 1 to 6 //
+    public static int rollDice(){
+        var rollNum = (int)(Math.random() * 6) + 1;
+        //System.out.println(rollNum);
+        return rollNum;
+    }
+    // Display the new numbers that were rolled //
+    public static void showRolls(int[] rolls, int[] saved){
+        var numInSave = checkSaved(saved);
+        System.out.print("\nNew: ");
+        if (numInSave != 0){System.out.print("\t Saved:");}
+        System.out.println();
+        // loop that displays the numbers
+        for (var rollNum : rolls){
+            if (rollNum != 0){
+                System.out.print(rollNum + " ");
+            }
+        }
+        if (numInSave != 0){ 
+            System.out.print("-- ");
+            for (var rollNum : saved){
+                if (rollNum != 0){
+                    System.out.print(rollNum + " ");
+                }
+            }
+        }
+        // Loop that prints out which number the roll is in
+        //System.out.println("\n===========");
+        //for (int i = 1; i <= newRolls.length; i++){
+        //    System.out.print(i + " ");
+        //}
+    }
+    // Let the user save any of the numbers they rolled. //
+    public static int[] saveRoll(int[] rolls, int[] saved, Scanner sc){
+        int responseNum = 10; // high number to start
+        do{
+            showRolls(rolls, saved);
+            System.out.print("\nWhich number would you like to save? (Enter '0' to move on): ");
+            try{responseNum = Integer.parseInt(sc.nextLine());}
+            catch (NumberFormatException e) {System.out.println("Invalid Response."); continue;}
+            //responseNum = Integer.parseInt(response);
+            for (int i = 0; i < rolls.length; i++){ // check each number in the roll array 
+                if (responseNum == rolls[i]){
+                    for (int j = 0; j < saved.length; j++){
+                        if (saved[j] == 0){
+                            saved[j] = responseNum; // replace the first available 0 in the saved array with the number.
+                            rolls[i] = 0;
+                            break; // prevent it from replacing all of the 0s.
+                        }
+                    }
+                    break; // prevent it from saving all the instances of the chosen number.
+                }
+            }
+        } while (responseNum != 0);
+        return saved;
+    }
+    // Let the user remove any values from the saved array //
+    public static int[] removeSavedNum(int[] saved, Scanner sc){
+        int responseNum = 10;
+        while (responseNum != 0 && (checkSaved(saved) != 0)){
+            System.out.print("\nWhich saved number would you like to remove?(Enter '0' to move on): ");
+            responseNum = Integer.parseInt(sc.nextLine());
+        }
+        return saved;
+    }
+    // Returns number of actual numbers are in the saved list // 
+    public static int checkSaved(int[] saved){
+        int count = 0;
+        for (var num : saved)
+            if (num != 0){
+                count += 1;
+            }
+        return count;
+    }
+    // combines the saved and last new roll :3 //
+    public static int[] combineArrays(int[] saved, int[] rolls){
+        int[] combined = {0, 0, 0, 0, 0};
+        for (var number : saved){ // checks each number in the saved list
+            if (number != 0){ // only replaces not zeros
+                for (int i = 0; i < combined.length; i++){ 
+                    if (combined[i] == 0){ // only replace the zeros in the combined list
+                        combined[i] = number;
+                        break;
+                    }
+                }
+                //break;
+            }
+        }
+        for (var number : rolls){
+            if (number != 0){
+                for (int i = 0; i < combined.length; i++){
+                    if (combined[i] == 0){
+                        combined[i] = number;
+                        break;
+                    }
+                }
+                //break;
+            }
+        }
+        return combined;
+    }
+}
---
+++ src/yacht_dice/scoring.java
@@ -0,0 +1,209 @@
+package yacht_dice;
+import java.util.Scanner;
+/**
+ * Scoring Calculations for Yacht Dice.
+ * @author Kat
+ * 
+ */
+
+// Aces, Deuces, Threes, Fours, Fives, Sixes
+// Subtotal Bonus of 35 if above 63
+
+// Choice, 4 of a Kind, Full House, S. Straight, L. Straight, Yacht
+// S. Straight: 15 Points, L Straight: 30 Points, Yacht: 50 Points
+// Total
+
+public class scoring {
+    // test for the scoring that uses a default number array //
+    public static void test(Scanner sc){
+        //int[] testNums = {1, 6, 3, 5, 3}; 
+        int[] testNums = {1, 2, 3, 4, 5}; 
+        boolean[] testStatus = {true, false, false, false, true, false, false, false, true, false, false, false};
+        generateScores(testNums, testStatus);
+        sc.nextLine();
+    }
+    // 
+    public static int[] generateScores(int[] dieRolls, boolean[] scoreStatus){
+        var numberPoints = checkNumbers(dieRolls);
+        var choicePoints = checkChoice(dieRolls);
+        var fourKindPoints = checkFourKind(dieRolls);
+        var fullHousePoints = checkFullHouse(dieRolls);
+        var straightPoints = checkStraights(dieRolls);
+        var yachtPoints = checkYacht(dieRolls);
+        var allScores = combineAllScores(numberPoints, choicePoints, fourKindPoints, fullHousePoints, straightPoints, yachtPoints);
+        displayAllScores(allScores, scoreStatus);
+        return allScores;
+    }
+    // Returns the totals of all the individual number combinations. //
+    private static int[] checkNumbers(int[] die){
+        int[] scoreArray = {0, 0, 0, 0, 0, 0};
+        for (int i = 1; i <= scoreArray.length; i++){
+            int count = 0;
+            for (var number : die){
+                if (number == i){
+                    count++;
+                }
+            }
+            //System.out.println(i + " " + count + " " + (i * count));
+            scoreArray[i - 1] = i * count;
+        }
+        //System.out.println(Arrays.toString(scoreArray));
+        return scoreArray;
+    }
+    // choice adds up all 5 die rolls //
+    private static int checkChoice(int[] die){
+        int score = 0;
+        for (var roll : die){
+            score += roll;
+        }
+        return score;
+    }
+    // check if there are four of a kind //
+    private static int checkFourKind(int[] die){
+        for (int i = 1; i <= 6; i++){
+            var count = countNum(i, die);
+            if (count >= 4){
+                return (i * 4);
+            }
+        }
+        return 0;
+    }
+    private static int checkFullHouse(int[] die){
+        for (int i = 1; i <= 6; i++){
+            var count = countNum(i, die);
+            if (count == 5){ // check if all 5 are the same. Technically counts lel
+                return (i * 5);
+            }
+            else if (count == 3){
+                int threeCount = i;
+                for (int j = 1; i <= 6; i++){
+                    if (count == 2 && count != threeCount){
+                        int twoCount = j;
+                        return ((twoCount * 2) + (threeCount * 3));
+                    }
+                        
+                }
+            }
+        }
+        return 0; // return nothing if doesnt meet criteria
+    }
+    // Straights //
+    private static int[] checkStraights(int[] die){
+        int[] straightScores = {0, 0};
+        sortArray(die);
+        int chain = 0;
+        int maxChain = 0;
+        for (int i = 0; i < die.length - 1; i++){
+            var difference = die[i + 1] - die[i];
+            if (difference == 1){
+                chain++;
+            }
+            else{
+                chain = 0;
+            }
+            if (chain > maxChain){
+                maxChain = chain;
+            }
+            //System.out.println("Chain: " + chain);
+            //System.out.println("Max Chain: " + maxChain);
+        }
+        //System.out.println("Final Max Chain: " + maxChain);
+        // Large Straight //
+        switch(maxChain){
+            default -> {straightScores[0] = 0; straightScores[1] = 0;}
+            case 4 -> {straightScores[0] = 15; straightScores[1] = 30;} // Large Straight 
+            case 3 -> {straightScores[0] = 15; straightScores[1] = 0;} // Small Straight
+        }
+            // 4 is max it can calculate
+        return straightScores;
+    }
+    // checks if all the die are the same for a yacht. //
+    private static int checkYacht(int[] die){
+        for (var rolls : die){
+            if (die[0] != rolls){
+                return 0;
+            }
+        }
+        return 50;
+    }
+
+
+    // returns the amount of times that a specific number is in the array //
+    private static int countNum(int numToCount, int[] rolls){
+        int count = 0;
+        for (var number : rolls){
+            if (numToCount == number){
+                count++;
+            }
+        }
+        return count;
+    }
+    // combines all the scores into one array //
+    private static int[] combineAllScores(int[] numScores, int choice, int fourKind, int fullHouse, int[] straights, int yacht){
+        int[] combinedScores = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        System.arraycopy(numScores, 0, combinedScores, 0, 6); // each of the top 6 numbered scores, also netbeans suggested this idk :3
+        combinedScores[6] = choice;
+        combinedScores[7] = fourKind;
+        combinedScores[8] = fullHouse;
+        System.arraycopy(straights, 0, combinedScores, 9, 2);
+        combinedScores[11] = yacht;
+        //System.out.println(Arrays.toString(combinedScores));
+        return combinedScores;
+    }
+    private static void displayAllScores(int[] numScores, boolean[] scoreUsed){
+        String[] sectionNames = {"Aces", "Deuces", "Threes", "Fours", "Fives", "Sixes", "Choice", "4 of a Kind", "Full House", "S. Straight", "L. Straight", "Yacht"};
+        for (int i = 0; i < sectionNames.length; i++){
+            String scoreNum;
+            if (scoreUsed[i] == true){
+                scoreNum = "--";
+            }
+            else{
+                scoreNum = "" + numScores[i];
+            }
+            System.out.printf("%n%s: %s", sectionNames[i], scoreNum);
+        }
+    }
+    // writes
+    public static String chooseScore(boolean[] scoreStatus, Scanner sc){
+        String[] sectionNames = {"aces", "deuces", "threes", "fours", "fives", "sixes", "choice", "4 of a kind", "full house", "s straight", "l straight", "yacht"};
+        do{
+            System.out.print("\nWhich Score would you like to save?(Enter the category name)(Entry is Case Sensitive): ");
+            var errMessage = "";
+            var catChoice = sc.nextLine();
+            for (int i = 0; i < sectionNames.length; i++){
+                if (catChoice.equalsIgnoreCase(sectionNames[i])){
+                    if (scoreStatus[i] == false){
+                        return catChoice;
+                    }
+                    else{
+                        errMessage = (sectionNames[i] + " already has a score. Try again..");
+                    }
+                }
+            }
+            if (errMessage.isEmpty()){
+                errMessage = "Category not Found. Try again..";
+            }
+            System.out.println(errMessage);
+        } while(true); 
+    }
+    // uses bubble sort to sort the array //
+    public static void sortArray(int arr[]){
+        int i, j, temp;
+        int length = arr.length;
+        boolean swapped;
+        for (i = 0; i < length - 1; i++){
+            swapped = false;
+            for (j = 0; j < length - i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    temp = arr[j]; // makes temp variable for the number
+                    arr[j] = arr[j + 1]; // replaces current with next
+                    arr[j + 1] = temp; // replaces next with current(temp)
+                    swapped = true;
+                }
+            }
+            if (!swapped){ // stops if doesnt swap
+                break;
+            }
+        }
+    }
+}
